21db472183ed8c197d2cfb634aef0428caa4d68f
Timothy Verhaeghe <me@timothyverhaeghe.com>
2018-07-01 16:18:40 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+
+
+const app = express();
+
+app.engine('html', ejs.renderFile);
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, 'views')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+
+app.listen(Number(process.env.PORT), () => {
+  console.log(`Website started on port ${Number(process.env.PORT)}`);
+});
